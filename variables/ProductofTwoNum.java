a67b173ca8191e5da6de87f98875abde5cb61ad3
gagandeep-singh19 <gagandeep.singh1926@gmail.com>
2025-11-28 03:43:34 -0800

    Add files via upload

---
+++ variables/ProductofTwoNum.java
@@ -0,0 +1,13 @@
+// Product of Two numbers 
+
+import java.util.*;
+public class ProductofTwoNum {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        float A = sc.nextFloat();
+        float B = sc.nextFloat();
+
+        float prod = A * B ;
+        System.out.println("Product of A & B :"+prod);
+    }
+}
