7019653ee63885c936e809b9e02ac7c29fb75fe0
gagandeep-singh19 <gagandeep.singh1926@gmail.com>
2025-11-28 03:38:24 -0800

    Add files via upload

---
+++ variables/SumofTwoNum.java
@@ -0,0 +1,12 @@
+// Sum of a &  b 
+import java.util.*;
+public class SumofTwoNum {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int sum = A + B;
+        
+        System.out.println("sum of A & B :" +sum);
+    }
+}
