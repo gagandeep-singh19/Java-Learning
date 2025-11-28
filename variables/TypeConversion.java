7d542ad7bb3bd4eb14535118fa5f82c8f9af97d5
gagandeep-singh19 <gagandeep.singh1926@gmail.com>
2025-11-28 03:48:04 -0800

    Add files via upload

---
+++ variables/TypeConversion.java
@@ -0,0 +1,10 @@
+// TYPE CONVERSION 
+
+import java.util.*;
+public class TypeConversion {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextInt();
+        System.out.println(number);
+    }
+}
