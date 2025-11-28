bb64995f4b60a0fda93c68269a8dfade247b2433
gagandeep-singh19 <gagandeep.singh1926@gmail.com>
2025-11-28 04:00:47 -0800

    Add files via upload

---
+++ variables/TypeCasting.java
@@ -0,0 +1,23 @@
+// TYPE CASTING 
+
+import java.util.*;
+public class TypeCasting {
+    public static void main(String[] args){
+        float a = 25.9f;
+        int b = (int)a;
+        System.out.println(b);
+
+        char ch = 'a';
+        int number = ch;
+        System.out.println(number);
+
+        float A = 27.9f;
+        int B = (int)A;
+        System.out.println(B);
+
+        double C = 289.98;
+        long D = (long)C;
+        System.out.println(C);
+
+    }
+}
